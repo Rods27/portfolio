858d04d748ee144739608bbd654c3ad8244be412
Rodrigo Leite <p22.rodrigo@webmotors.com.br>
2023-11-02 17:07:46 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import type { NextPage } from 'next';
+import Header from '@/components/Header';
+
+const Home: NextPage = () =>{
+  return (
+    <div>
+      <Head>
+        <title>Rod``&apos`s Portfolio</title>
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact */}
+    </div>
+  )
+}
+
+export default Home;
